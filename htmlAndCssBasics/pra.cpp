d6865a4a22376c840eb185796331b44a0136bcef
Prajwal <prajwalgowda93535@gmail.com>
2024-09-18 02:05:17 +0530

    first commit

---
+++ htmlAndCssBasics/pra.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int a = 20;
+    int b = 10;
+    cout << a << " " << b << endl;
+    a = a + b;
+    b = a - b;
+    a = a - b;
+    cout << a << " " << b << endl;
+
+    return 0;
+}
